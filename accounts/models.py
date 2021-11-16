af9be7b9d4a7597e098e5ab42dd79ca53e09c6b4
aletsire <aletsire@gmail.com>
2021-11-16 15:01:37 +0900

    accounts_models_confirm

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    email = models.CharField(max_length=100)
--- movie_pjt/settings.py
+++ movie_pjt/settings.py
@@ -138,7 +138,7 @@ AUTH_USER_MODEL = 'accounts.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
