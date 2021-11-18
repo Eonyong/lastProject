ea87a1d5c004f0a3dd10101634b16cdb16cb13bb
Eonyong <unjoo94@naver.com>
2021-11-18 09:13:41 +0900

    vue 수정

--- movie_pjt_vue/src/App.vue
+++ movie_pjt_vue/src/App.vue
@@ -1,39 +1,62 @@
 <template>
   <div id="app">
-    <nav id="nav" class="navbar-expand-md align-items-center flex-column fixed-top bg-none">
+    <nav id="nav" class="navbar-expand-md item-align-center flex-column fixed-top bg-transparent">
+      <!-- side padding nav 제작 -->
+      <b-sidebar id="sidebar-1" class="bg-transparent"
+      aria-labelledby="sidebar-no-header-title"
+      backdrop backdrop-variant="transparent"
+      no-header shadow>
+        <!-- 오른쪽 컴포넌트 제작 -->
+        <b-button-group vertical class="d-flex" style="text-align: center;">
+          <!-- <li class="nav-item"><router-link to="/"> Home </router-link></li> -->
+          <b-button class="btn btn-link" :to="{path: '/accounts/login'}"
+          v-if="login" variant="link">
+            로 그 인
+          </b-button>
+          
+          <b-button class="btn btn-link" :to="{path: '/accounts/signup'}"
+          v-if="login" variant="link">
+            회 원 가 입
+          </b-button>
+
+          <b-button class="btn btn-link" v-else
+          @click.native="logout" variant="link">
+            로 그 아 웃
+          </b-button>
+
+          <b-button class="btn btn-link"
+          :to="{path: '/'}" variant="link">
+            영 화 목 록
+          </b-button>
+
+          <b-button class="btn btn-link"
+          :to="{path: '/accounts/signup'}" variant="link">
+            모 든 리 뷰 모 아 보 기
+          </b-button>
+        </b-button-group>
+      </b-sidebar>
       <!-- Home 컴포넌트 제작 -->
       <div class="container-fluid d-flex">
-        <a href="/" class="navbar-brand">Home</a>
+        <b-button v-b-toggle.sidebar-no-header v-b-toggle.sidebar-1 class="bg-transparent">
+          <b-icon-list/>
+          <!-- <img :src="require('@/assets/list_icon.png')" style="image-rendering: auto;" alt="" v-b-toggle.sidebar-1 width="32" height="32"> -->
+        </b-button>
         <!-- Search bar 제작 -->
         <div class="input-group ps-5 ">
           <div id="navbar-search-autocomplete" class="form-outline">
-            <input type="search" id="form1" class="form-control" placeholder="Search"/>
+            <input type="search" id="form1" class="form-control bg-transparent" placeholder="Search"/>
           </div>
         </div>
-        <!-- 오른쪽 컴포넌트 제작 -->
-        <div>
-          <ul class="navbar-nav flex-row">
-            <li class="nav-item"><router-link to="/"> Home </router-link></li>
-            <span class="d-flex" v-if="login">
-              <p class="px-2">|</p>
-              <li class="nav-item"><router-link to="/accounts/login"> 로그인 </router-link></li>
-              <p class="px-2">|</p>
-              <li class="nav-item"><router-link to="/accounts/signup"> 회원가입 </router-link></li>
-            </span>
-            <span v-else>
-              <p class="px-2">|</p>
-              <li class="nav-item"><router-link @click.native="logout" to="#"> 로그아웃 </router-link></li>
-            </span>
-          </ul>
-        </div>
-        <!-- 오른쪽 컴포넌트 마지막 -->
       </div>
+      
     </nav>
     <router-view @login="login = false"/>
+    
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'App',
   data: function () {
@@ -41,11 +64,13 @@ export default {
       login: false,
     }
   },
+
   methods: {
+
     logout: function () {
       localStorage.removeItem('jwt')
       this.$router.push({ name: 'Login' })
-    }
+    },
     
   },
   created: function () {
@@ -84,4 +109,9 @@ export default {
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+a {
+  text-decoration: none;
+}
+
 </style>
--- movie_pjt_vue/src/views/Home.vue
+++ movie_pjt_vue/src/views/Home.vue
@@ -1,108 +1,64 @@
+      
+      <!-- @sliding-start="onSlideStart" -->
+      <!-- @sliding-end="onSlideEnd" -->
 <template>
-  <div class="home">
-    <!-- Carousel wrapper -->
-    <div
-      id="carouselDarkVariant"
-      class="carousel slide carousel-fade carousel-dark"
-      data-mdb-ride="carousel"
+  <div>
+    <b-carousel
+      id="carousel-1"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="360"
+      fade
     >
-      <!-- Indicators -->
-      <div class="carousel-indicators">
-        <button
-          data-mdb-target="#carouselDarkVariant"
-          data-mdb-slide-to="0"
-          class="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          data-mdb-target="#carouselDarkVariant"
-          data-mdb-slide-to="1"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          data-mdb-target="#carouselDarkVariant"
-          data-mdb-slide-to="2"
-          aria-label="Slide 1"
-        ></button>
-      </div>
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        img-src="https://picsum.photos/1024/360/?image=52"
+      ></b-carousel-slide>
 
-      <!-- Inner -->
-      <div class="carousel-inner">
-        <!-- Single item -->
-        <div class="carousel-item active">
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/360/?image=54">
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/360/?image=58"></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template #img>
           <img
-            src="https://mdbootstrap.com/img/Photos/Slides/img%20(19).jpg"
-            class="d-block w-100"
-            alt="..."
-          />
-          <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </div>
-        </div>
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="360"
+            src="https://picsum.photos/1024/360/?image=55"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
 
-        <!-- Single item -->
-        <div class="carousel-item">
-          <img
-            src="https://mdbootstrap.com/img/Photos/Slides/img%20(35).jpg"
-            class="d-block w-100"
-            alt="..."
-          />
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-        </div>
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+      </b-carousel-slide>
+    </b-carousel>
 
-        <!-- Single item -->
-        <div class="carousel-item">
-          <img
-            src="https://mdbootstrap.com/img/Photos/Slides/img%20(40).jpg"
-            class="d-block w-100"
-            alt="..."
-          />
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </div>
-        </div>
-      </div>
-      <!-- Inner -->
-
-      <!-- Controls -->
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-mdb-target="#carouselDarkVariant"
-        data-mdb-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-mdb-target="#carouselDarkVariant"
-        data-mdb-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-    <!-- Carousel wrapper -->
-
-    <!-- movie wrapper -->
     <section class="list item-more" id="movieListExample1" style="padding-top: 0px;">
       <h2 class="section-title">인기 영화</h2>
       <div class="bx-wrapper" style="max-width: 1200px;">
         <div data-min="6" data-max="6" data-margin="18" data-pager="true" data-controls="true" data-noslide="true"
         class="bxslider bxslider-in" style="width: 20215% position: relative; transition-duration: 0s; transform: translated3d(0px, 0px, 0px);">
           <!-- movie image list 출력  -->
+          <div num="false" data-page-no class="column-6" >
+
+          </div>          
           <!-- single item -->
         </div>
       </div>
     </section>
+
+    
   </div>
 </template>
 
@@ -111,6 +67,20 @@
 export default {
   name: 'Home',
   components: {
+  },
+  // data () {
+  //   return {
+  //     slide: 0,
+  //     sliding: null,
+  //   }
+  // },
+  methods: {
+    // onSlideStart(slide) {
+    //   this.sliding = true
+    // },
+    // onSlideEnd(slide) {
+    //   this.sliding = false
+    // }
   }
 }
 </script>
